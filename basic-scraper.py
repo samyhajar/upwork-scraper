502e557fe059dd614fe3585a58ff1e5fa54598e0
samyhajar <samy.hajar@gmail.com>
2022-08-14 15:15:15 +0200

    defined function to parse products

--- basic-scraper.py
+++ basic-scraper.py
@@ -1,10 +1,39 @@
+from math import prod
 from requests_html import HTMLSession
 
 
 s = HTMLSession()
 
-url = 'https://themes.woocommerce.com/storefront/product-category/clothing/page/1'
 
-r = s.get(url)
+def get_product_links(page):
+    url = f'https://themes.woocommerce.com/storefront/product-category/clothing/page/{page}'
 
-print(r.html.find())
+    links = []
+    r = s.get(url)
+    products = r.html.find('ul.products li')
+    for item in products:
+      links.append(item.find('a', first=True).attrs['href'])
+    return links
+
+page1 = get_product_links(1)
+#print(page1)
+
+def parse_products(url):
+
+    r = s.get(url)
+
+    title = r.html.find('h1.product_title.entry-title' , first=True).text.strip()
+    price = r.html.find('p.price' , first=True).text.strip()
+    sku = r.html.find('span.sku' , first=True).text.strip()
+    cat = r.html.find('span.posted_in' , first=True).text.strip()
+
+
+    product = {
+        'title' : title,
+        'price' : price,
+        'sku' : sku,
+        'cat' : cat,
+    }
+    return product
+
+print(parse_products('https://themes.woocommerce.com/storefront/product/red-checked-shirt/'))
